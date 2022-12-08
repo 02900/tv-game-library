a62187d28316ec2ecd12c51ae78126de2d79a3ba
Juan Ortiz <jotaatepe@gmail.com>
2022-12-07 19:17:52 -0600

    feat: switch to hdmi

--- app/src/main/java/com/example/tvgamelibrary/MainActivity.java
+++ app/src/main/java/com/example/tvgamelibrary/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.tvgamelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.tv.TvContract;
+import android.media.tv.TvInputInfo;
+import android.media.tv.TvInputManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
-    @Override
+public class MainActivity extends Activity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void switchToHDMI(View view) {
+        Context context = view.getContext();
+        TvInputManager tim = (TvInputManager) context.getSystemService(Context.TV_INPUT_SERVICE);
+        List<TvInputInfo> a = tim.getTvInputList();
+        String msg = "Not info about TV Input";
+
+        if (!a.isEmpty()) {
+            for(int i = 0; i<a.size(); i++) {
+
+                TvInputInfo item = a.get(i);
+                if (item.loadLabel(context).toString() == "HDMI 1") {
+
+                    Uri inputInfoIdUri =
+                            TvContract.buildChannelUriForPassthroughInput(item.getId());
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(inputInfoIdUri);
+                    context.startActivity(intent);
+
+                    break;
+                }
+
+                msg += "<${this.context?.let { item.loadLabel(it).toString() }}> ,";
+            }
+        }
+
+        Log.d("Hey brou, it works!", msg);
     }
 }
